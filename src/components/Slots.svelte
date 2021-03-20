c45e1c8ac0553400a65b9f3bf7183aa4953e1e1a
Sean Lim <seanlimdev856@gmail.com>
2021-03-20 12:25:33 +0800

    load slots

--- src/components/Slots.svelte
+++ src/components/Slots.svelte
@@ -1,3 +1,26 @@
+<script>
+  import { onMount } from "svelte";
+  import fetch from "unfetch";
+
+  const parseDate = (date) => new Date(date);
+
+  let slots = null;
+  const fetchSlots = () => {
+    fetch("https://triomic.github.io/climbing-gym-scraper/sessions.json")
+      .then((r) => r.json())
+      .then((s) => slots = s.data);
+  };
+
+  onMount(fetchSlots);
+</script>
+
 <div>
   <h1>Slots</h1>
+    {#if slots === null}
+      <p>Loading...</p>
+    {:else}
+      {#each slots as slot}
+        <p>{slot.gym} {parseDate(slot.start)} to {parseDate(slot.end)}, available slots: {slot.slots}</p>
+      {/each}
+    {/if}
 </div>
