4cb0a21284167ae847fd5eabf9528170ebe1d733
Sean Lim <seanlimdev856@gmail.com>
2021-03-21 16:53:43 +0800

    render by gym and date

--- src/components/Slots.svelte
+++ src/components/Slots.svelte
@@ -1,14 +1,32 @@
 <script>
   import { onMount } from "svelte";
   import fetch from "unfetch";
+  import groupBy from "lodash/groupBy";
+  import sortBy from "lodash/sortBy";
 
-  const parseDate = (date) => new Date(date);
+  import { getDateString, getTimeString } from "../utils/date";
 
-  let slots = null;
+  let data = null;
   const fetchSlots = () => {
     fetch("https://triomic.github.io/climbing-gym-scraper/sessions.json")
       .then((r) => r.json())
-      .then((s) => slots = s.data);
+      .then((slots) =>
+        slots.data.map((slot) => ({
+          ...slot,
+          timing: `${getTimeString(new Date(slot.start))} to ${getTimeString(
+            new Date(slot.end)
+          )}`,
+          date: getDateString(new Date(slot.start)),
+        }))
+      )
+      .then((slots) => groupBy(sortBy(slots, "start"), "gym"))
+      .then((gyms) => {
+        data = {};
+        Object.keys(gyms).forEach((gym) => {
+          data[gym] = groupBy(gyms[gym], "date");
+        });
+        console.info(data);
+      });
   };
 
   onMount(fetchSlots);
@@ -16,11 +34,19 @@
 
 <div>
   <h1>Slots</h1>
-    {#if slots === null}
-      <p>Loading...</p>
-    {:else}
-      {#each slots as slot}
-        <p>{slot.gym} {parseDate(slot.start)} to {parseDate(slot.end)}, available slots: {slot.slots}</p>
+  {#if data === null}
+    <p>Loading...</p>
+  {:else}
+    {#each Object.keys(data) as gym}
+      <b>{gym.toUpperCase()}</b>
+      {#each Object.keys(data[gym]) as date}
+        {date}
+        {#each data[gym][date] as slot}
+          <p>
+            {slot.timing}, available spaces: {slot.spaces}
+          </p>
+        {/each}
       {/each}
-    {/if}
+    {/each}
+  {/if}
 </div>
---
+++ src/utils/date.js
@@ -0,0 +1,27 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export const getDateString = (date) =>
+  `${date.getDate()} ${months[date.getMonth()]} ${days[date.getDay()]}`;
+
+export const getTimeString = (date) => {
+  return date.toLocaleString("en-SG", {
+    hour: "numeric",
+    hour12: true,
+    minute: "numeric",
+    minute12: true,
+  });
+};
