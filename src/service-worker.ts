249df0bfc756c5e4334115d3ef252c70c4788b51
Sean Lim <seanlimdev856@gmail.com>
2021-09-10 07:56:45 +0800

    add fetch listener to sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -15,3 +15,36 @@ self.addEventListener("install", function (event) {
     );
   });
 });
+
+self.addEventListener("fetch", function (event) {
+  // skip requests for extensions
+  if (!(event.request.url.indexOf("http") === 0)) return;
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        let responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
