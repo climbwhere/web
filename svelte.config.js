3b25f75582b96199075476f6d161942c268f2a57
Sean Lim <seanlimdev856@gmail.com>
2021-09-10 07:35:01 +0800

    switch to node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-netlify";
+// import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +10,7 @@ const config = {
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
-    adapter: adapter(),
+    adapter: adapter({ env: { port: process.env.PORT } }),
     target: "#svelte",
   },
 };
